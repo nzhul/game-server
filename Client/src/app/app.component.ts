1d74e0b9b373d1d93814923430d018196ce4bac9
Dobromir Ivanov <dobromirivanov1@gmail.com>
2018-04-26 17:51:41 +0300

    up

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './_services/auth.service';
+import { User } from './_models/User';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  constructor(private authService: AuthService, private jwtHelperService: JwtHelperService) { }
+
+  ngOnInit() {
+      const token = localStorage.getItem('token');
+      const user: User = JSON.parse(localStorage.getItem('user'));
+      if (token) {
+          this.authService.decodedToken = this.jwtHelperService.decodeToken(token);
+      }
+      if (user) {
+          this.authService.currentUser = user;
+          if (this.authService.currentUser.photoUrl != null) {
+              this.authService.changeMemberPhoto(user.photoUrl);
+          } else {
+              this.authService.changeMemberPhoto('../assets/user.png');
+          }
+      }
+  }
+
 }
