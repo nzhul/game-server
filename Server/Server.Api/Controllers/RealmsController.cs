f9d5a83380c64f8143efbe41146104b179cef230
nzhul <dobromirivanov1@gmail.com>
2018-06-17 23:50:58 +0300

    update

---
+++ Server/Server.Api/Controllers/RealmsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Server.Data.Services.Abstraction;
+
+namespace Server.Api.Controllers
+{
+    public class RealmsController : Controller
+    {
+        private readonly IRealmsService _realmsService;
+        private readonly IMapper _mapper;
+
+        public RealmsController()
+        {
+
+        }
+    }
+}
