44cbbbd9e641b6ae1c40ed0473971f69e8194c3a
nzhul <dobromirivanov1@gmail.com>
2021-07-10 13:31:33 +0300

    cleanup and update submodule ref

--- GameServer/GameServer/PacketHandlers/RestRequestHandler.cs
+++ GameServer/GameServer/PacketHandlers/RestRequestHandler.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using Assets.Scripts.Network.NetworkShared.Packets.World.ClientServer;
-using GameServer.Managers;
+﻿using GameServer.Managers;
 using NetworkingShared;
 using NetworkingShared.Attributes;
 
@@ -11,16 +9,9 @@ namespace GameServer.PacketHandlers
     {
         public void Handle(INetPacket packet, int connectionId)
         {
-            var msg = (Net_RestRequest)packet;
-            Console.WriteLine($"Received {nameof(msg)} packet!");
-
             var connection = NetworkServer.Instance.Connections[connectionId];
             connection.User.Avatar.IsResting = true;
             GameManager.Instance.DoNewDayRestCheck(connection.UserId);
-
-            // 1. Mark avatar.TurnConsumed = true
-            // 2. foreach all avatars in game and check if all TurnConsumed = true
-            // 3. If true -> raise NewDay event.
         }
     }
 }
